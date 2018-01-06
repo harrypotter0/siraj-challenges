3d0b20a408ee22af90fec127fa8e7b2589c6d67c
harrypotter0 <9654263057akashkandpal@gmail.com>
2018-01-06 18:31:38 +0530

    flower(iris) classifier using tensorflow

---
+++ iris-classifier.py
@@ -0,0 +1,14 @@
+
+import tensorflow as tf
+import tensorflow.contrib.learn as skflow
+from sklearn import datasets
+
+iris = datasets.load_iris()
+feature_columns = [tf.contrib.layers.real_valued_column("", dimension=4)]
+
+classifier = skflow.DNNClassifier(feature_columns=feature_columns, hidden_units=[10, 20, 10], n_classes=3)
+
+classifier.fit(iris.data, iris.target, steps=50)
+
+score = classifier.evaluate(iris.data, iris.target, steps=1)["accuracy"]
+print("Accuracy: {}".format(score))
